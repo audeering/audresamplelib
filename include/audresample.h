1aa6e3ccfd496071a5f647d54007a36b6937a0cf
Andrea Crespi <acrespi@audeering.com>
2020-01-14 09:37:22 +0100

    better handling of I/O data types

--- include/audresample.h
+++ include/audresample.h
@@ -1,5 +1,6 @@
 #include <soxr.h>
 #include <audeering/commonlib-audeering.h>
+#include <stdlib.h>
 
 typedef struct {
   double srIn;
@@ -11,6 +12,8 @@ t_converter_config * init_converter_config(double srIn, double srOut);
 void free_converter_config(t_converter_config *converterConfig);
 
 size_t get_output_length(size_t inLen, t_converter_config *converterConfig);
+
+soxr_io_spec_t to_sox_type(size_t dataSize);
 
 void audresample_oneshot(
     t_converter_config *converterConfig,
