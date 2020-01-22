9993ce82c23f8e3fe69d98fa3ba080d31c83d532
Andrea Crespi <acrespi@audeering.com>
2020-01-22 16:11:57 +0100

    add some doc strings

--- include/audresample.h
+++ include/audresample.h
@@ -8,6 +8,16 @@ typedef struct {
   soxr_quality_spec_t quality;
 } t_converter_config;
 
+
+/** Configure the converter.
+ *
+ * The \p quality parameter is directly mapped to one of the 5 standard quality
+ * recipes provided by soxr:
+ * * q: quick
+ * * l: low
+ * * m: medium
+ * * h: high
+ * * v: very high */
 t_converter_config init_converter_config(
     double srIn, double srOut, char quality
 );
